947635158dc4c577ddf0ed7cf0d3f8445f6fab34
arczhh <34346210+arczhh@users.noreply.github.com>
2020-02-18 17:02:43 +0700

    add alert func for showdetail.php

--- pre-website/js/custom_script.js
+++ pre-website/js/custom_script.js
@@ -1,3 +1,15 @@
 function goBack() {
     window.history.back();
 }
+function statusChangeConfirm() {
+    alert("เปลี่ยนสถานะเสร็จสิ้น");
+}
+function changeLoc() {
+    alert("เปลี่ยนสถานที่เก็บอุปกรณ์สำเร็จ");
+}
+function changeCat() {
+    alert("เปลี่ยนชนิดอุปกรณ์สำเร็จ");
+}
+function changeEquipDetail() {
+    alert("บันทึกสำเร็จ");
+}
